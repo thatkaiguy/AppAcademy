7e1c248ab2835b818c7f3f361633b1b2c4b41320
App Academy Student <student@appacademy.io>
2015-07-21 17:26:00 -0700

    phase V

--- Week5/w5d2/rails_lite/lib/phase5/params.rb
+++ Week5/w5d2/rails_lite/lib/phase5/params.rb
@@ -1,4 +1,5 @@
 require 'uri'
+require 'byebug'
 
 module Phase5
   class Params
@@ -10,9 +11,34 @@ module Phase5
     # You haven't done routing yet; but assume route params will be
     # passed in as a hash to `Params.new` as below:
     def initialize(req, route_params = {})
+      @params = {}
+
+      query_params = {}
+      unless req.query_string.nil?
+        query_params = parse_www_encoded_form(req.query_string)
+      end
+
+      body_params = {}
+      unless req.body.nil?
+        body_params = parse_www_encoded_form(req.body)
+      end
+
+      merged_params = {}
+      [route_params, query_params, body_params].each do |param_hash|
+        merged_params.merge!(param_hash)
+      end
+
+      merged_params.each do |k,v|
+        self[k] = v
+      end
     end
 
     def [](key)
+      @params[key.to_sym]
+    end
+
+    def []=(key, val)
+      @params[key.to_sym] = val
     end
 
     # this will be useful if we want to `puts params` in the server log
@@ -29,11 +55,40 @@ module Phase5
     # should return
     # { "user" => { "address" => { "street" => "main", "zip" => "89436" } } }
     def parse_www_encoded_form(www_encoded_form)
+      params = {}
+      #['user', 'address', 'street']
+      #{user:{}}
+      #{user: { address:{} }}
+      #{user: { address: {street: 'blah st'}}
+      URI::decode_www_form(www_encoded_form).each do |entry|
+        keys = parse_key(entry.first)
+        curr_hash = nil
+        if keys.count > 1
+          #nested keys
+          keys.take(keys.count - 1).each do |key|
+            if curr_hash
+              curr_hash[key] ||= {}
+              curr_hash = curr_hash[key]
+            else
+              params[key] ||= {}
+              curr_hash = params[key]
+            end
+          end
+          curr_hash[keys.last] = entry.last
+        elsif keys.count == 1
+          #top level
+          params[keys.last] = entry.last
+        else
+          raise "no params"
+        end
+      end
+      params
     end
 
     # this should return an array
     # user[address][street] should return ['user', 'address', 'street']
     def parse_key(key)
+      key.split(/\]\[|\[|\]/)
     end
   end
 end
